84608a28e7d2ad3a480851a813b6ccf0f7171653
nosyarg18 <gbudin2@huskers.unl.edu>
2026-08-27 10:06:02 -0500

    fix

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
  * Author: Grayson Budin
  *
- * Date: 2026/08/25
+ * Date: 2026/08/27
  *
- * A simple hello world program in C
+ * A simple program in C
  *
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-printf("Grayson Budin\n");
-
+printf("Grayson Budin!\n");
+printf("Computer Engineering\n");
   return 0;
 }
