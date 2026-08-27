e325bb6e63f6331e7e5a1f3f832898ad756181e0
nosyarg18 <gbudin2@huskers.unl.edu>
2026-08-27 09:41:18 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Grayson Budin
+ *
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+printf("Grayson Budin\n");
+
+  return 0;
+}
